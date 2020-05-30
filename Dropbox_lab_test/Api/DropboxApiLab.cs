97f21d93b2b4226d95c2d112d674d97281299430
vladk <vladkarachov@gmail.com>
2020-05-30 18:22:56 +0300

    restore

---
+++ Dropbox_lab_test/Api/DropboxApiLab.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using TestDropboxApi.ApiFacade;
+using TestDropboxApi.Builders;
+using TestDropboxApi.DataModels;
+using TestDropboxApi.Helpers;
+
+namespace Dropbox_lab_test.Api
+{
+    class DropboxApiLab: DropboxApiContent //наследуем целую 1 переменную и переделываем конструктор. ООП, конечно, серьезное
+    {
+        public DropboxApiLab()
+        {
+            request = new RequestBuilder(ConfigurationHelper.ServiceUrl);
+        }
+        public ApiResponse DeleteFile(string path)
+        {
+            var Body = new Base();
+            Body.Path = path;
+            var requestBody = JsonConvert.SerializeObject(Body);
+            var url = "files/delete_v2";
+            return request.Uri(url).Method(HttpMethod.Post)
+                .WithBody(requestBody)
+                 .Execute();
+        }
+        public ApiResponse GetFileMeta(string path)
+        {
+            var Body = new Base();
+            Body.Path = path;
+            var requestBody = JsonConvert.SerializeObject(Body);
+            var url = "files/get_metadata";
+            return request.Uri(url).Method(HttpMethod.Post)
+                .WithBody(requestBody)
+                 .Execute();
+        }
+
+       
+    }
+}
